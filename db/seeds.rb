d7be579504a13c9e5cd4e45d6c3d3cad8adbf307
Romain Buisine rom.buisine@gmail.com <rom.buisine@gmail.com>
2023-06-05 16:22:26 +0200

    seeds_flat

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Flat.create([{}])
+
+flat1 = Flat.create(address: "Place de l'église", city: "Lyon", number_of_guests: "2", price_per_night: "56", description: "Très jolie")
+flat2 = Flat.create(address: "Rue la gare", city: "Marseille", number_of_guests: "4", price_per_night: "75", description: "Cosy")
