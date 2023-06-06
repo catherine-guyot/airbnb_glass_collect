4a810ba47dac6c837667cfd4581b304441623e4a
Romain Buisine rom.buisine@gmail.com <rom.buisine@gmail.com>
2023-06-06 12:42:55 +0200

    new_flat

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "flats#index"
-  resources :flats, only: [:new, :create]
+  resources :flats, only: [:show, :new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
