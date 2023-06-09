3e8e27fb3e59b9ee4d96bf2d678db7fdb17be362
adeline balian <ad.balian@laposte.net>
2023-06-09 16:34:33 +0200

    new_booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,14 +1,31 @@
 class BookingsController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @booking = Booking.all
+  end
+
   def new
+    @flat = Flat.find(params[:flat_id])
     @booking = Booking.new
   end
-  # def create
 
-  # end
-  # def show
+  def show
+    @booking = Booking.find(params[:id])
+  end
 
-  # end
-  # def destroy
+  def create
+    @flat = Flat.find(params[:flat_id])
+    @booking = Booking.new(booking_params)
+    @booking.flat = @flat
+    @booking.user = current_user
+    @booking.status = "A valider"
+    @booking.save
+    redirect_to flat_booking_path(@flat, @booking)
+  end
 
-  # end
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :status, :booking_price)
+  end
 end
