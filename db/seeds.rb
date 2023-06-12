56bbf3df285dced07fb43bc2f9f4da17caad91c6
adeline balian <ad.balian@laposte.net>
2023-06-12 19:49:23 +0200

    ssl

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# Flat.destroy_all
+# User.destroy_all
 
-User.destroy_all
-Flat.destroy_all
 
-user1 = User.create(email:"user1@gmail.com", password:"azerty")
+# user1 = User.create(email:"user1@gmail.com", password:"azerty")
 
- flat1 = Flat.create(address: "Place de l'église", city: "Lyon", number_of_guests: "2", price_per_night: "56", description: "Très jolie", user_id:user1.id)
- flat2 = Flat.create(address: "Rue la gare", city: "Marseille", number_of_guests: "4", price_per_night: "75", description: "Cosy", user_id:user1.id)
+#  flat1 = Flat.create(address: "Place de l'église", city: "Lyon", number_of_guests: "2", price_per_night: "56", description: "Très jolie", user_id:user1.id)
+#  flat2 = Flat.create(address: "Rue la gare", city: "Marseille", number_of_guests: "4", price_per_night: "75", description: "Cosy", user_id:user1.id)
